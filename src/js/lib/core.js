fa153fe9e140917ef1f04e8075ee40255f465674
dmitriypetukhov <pdnsite@gmail.com>
2021-07-10 13:17:25 +0300

    Сборка и архитектура проекта

--- src/js/lib/core.js
+++ src/js/lib/core.js
@@ -1,35 +1,20 @@
-// (() => {
-// 	const $ = (selector) => {
-// 		const elements = document.querySelectorAll(selector);
-// 		const obj = {};
-
-// 		obj.hide = () => {
-// 			elements.forEach(elem => {
-// 				elem.style.display = 'none';
-// 			});
-// 			return obj;
-// 		};
-
-// 		obj.show = () => {
-// 			elements.forEach(elem => {
-// 				elem.style.display = '';
-// 			});
-// 			return obj;
-// 		};
-
-// 		return obj;
-// 	};
-
-// 	window.$ = $;
-// })();
-
-const $ = (selector) => {
+const $ = function(selector) {
 	return new $.prototype.init(selector);
 };
 
 $.prototype.init = function(selector) {
 	if (!selector) {
-		return this; // {}
+		return this;
 	}
-	document.querySelectorAll(selector);
+
+	Object.assign(this, document.querySelectorAll(selector));
+	this.length = document.querySelectorAll(selector).length;
+
+	return this;
 };
+
+$.prototype.init.prototype = $.prototype;
+
+window.$ = $;
+
+export default $;
