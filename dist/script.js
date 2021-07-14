31bc0171af56978e87fdeed421d25f2a1235baac
dmitriypetukhov <pdnsite@gmail.com>
2021-07-14 19:02:51 +0300

    Проект только с использованием нашей библиотеки

--- dist/script.js
+++ dist/script.js
@@ -894,52 +894,6 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.post = async function (u
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _lib_lib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./lib/lib */ "./src/js/lib/lib.js");
 
-$('#first').click(() => {
-  $('div').eq(3).fadeToggle(800);
-});
-$('[data-count="second"]').click(() => {
-  $('div').eq(4).fadeToggle(800);
-});
-$('.btn').eq(2).on('click', () => {
-  $('.w-500').fadeToggle(800);
-});
-$('.wrap').html(`
-	<div class="dropdown m20">
-	<button class="btn btn-success dropdown-toggle" id="dropdownMenuButton3">Открыть dropdown меню</button>
-
-	<ul class="dropdown-menu" data-toggle-id="dropdownMenuButton3">
-		<li><a href="#" class="dropdown-item">Пункт 1</a></li>
-		<li><a href="#" class="dropdown-item">Пункт 2</a></li>
-		<li><a href="#" class="dropdown-item">Пункт 3</a></li>
-	</ul>
-	</div>
-`);
-$('.dropdown-toggle').dropdown();
-$('[data-showmodal-id]').modalOpen();
-$('[data-close]').modalClose(); // $('.btn-card').eq(0).click(() => {
-// 	$('.btn-card').eq(0).createModal({
-// 		text: {
-// 			title: 'Modal title 1',
-// 			body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate a modi, nisi magnam odit atque exercitationem eligendi doloribus? Quidem, reprehenderit.'
-// 		},
-// 		btns: {
-// 			count: 2,
-// 			settings: [
-// 				[ 'Save changes', ['btn-success', 'mr-20'], false ],
-// 				[ 'Close', ['btn-danger'], true ],
-// 				() => {
-// 					alert('Данные сохранены');
-// 				}
-// 			]
-// 		}
-// 	});
-// });
-// $().get('https://jsonplaceholder.typicode.com/todos/1')
-// 	.then(res => console.log(res));
-
-$().post('https://jsonplaceholder.typicode.com/posts', JSON.stringify({
-  name: 'Dima'
-})).then(json => console.log(json));
 
 /***/ })
 
---
+++ test-project/js/script.js
@@ -0,0 +1,2 @@
+$('[data-showmodal-id]').modalOpen();
+$('[data-close]').modalClose();
